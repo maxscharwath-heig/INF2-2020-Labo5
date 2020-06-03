e1b84b0d3ab960a4e4e6566d8af96de37bc099ea
Klab93 <37708074+Klab93@users.noreply.github.com>
2020-06-03 16:57:38 +0200

    fonction afficher bateau

--- Bateau.c
+++ Bateau.c
@@ -54,15 +54,34 @@ void afficherBateau(Bateau bateau) {
          printf(
                "Type : Bateau de peche \n"
                "Nom  : %s \n"
-               "capacite  : %s \n"
-               , bateau.nom
+               "Puissance  : %" PRIu16 "CV\n"
+               "Capacite   : %" PRIu8 "t\n\n",
+               bateau.nom,
+               bateau.bateauMoteur.puissance,
+               bateau.bateauMoteur.type.bateauPeche.capacite
                );
          break;
       case T_PLAISANCE :
-         printf("Type : Bateau de plaisance \n");
+         printf(
+               "Type : Bateau de plaisance \n"
+               "Nom  : %s \n"
+               "Nom du proprietaire : %s \n"
+               "Puissance  : %" PRIu16 "CV\n"
+               "Longueur   : %" PRIu8 "m\n\n",
+               bateau.nom,
+               bateau.bateauMoteur.type.bateauPlaisance.nomProprietaire,
+               bateau.bateauMoteur.puissance,
+               bateau.bateauMoteur.type.bateauPlaisance.longueur
+               );
          break;
       case T_VOILIER :
-         printf("Type : Voilier\n");
+         printf(
+               "Type : Voilier\n"
+               "Nom  : %s \n"
+               "Surface de la voile  : %" PRIu16 "m2\n\n",
+               bateau.nom,
+               bateau.voilier.voilure
+               );
          break;
    }
 
